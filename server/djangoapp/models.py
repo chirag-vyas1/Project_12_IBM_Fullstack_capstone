717f6ce0c8991d9a721f3003f087dca4c3e34cb5
chirag-vyas1 <chiragvyas1086@yahoo.com>
2025-02-28 09:31:33 -0500

    8th commit

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,12 +1,19 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)  # Optional field
+    manufactured_year = models.PositiveIntegerField(blank=True, null=True)  # Optional field
 
+    def __str__(self):
+        return f"{self.name} - {self.country} (year: {self.manufactured_year})"# Returns only the name as the string representation
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
@@ -15,6 +22,35 @@
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name="models")  
+    name = models.CharField(max_length=100)
+    dealer_id = models.IntegerField()  # Refers to a dealer in Cloudant
+
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('COUPE', 'Coupe'),
+        ('CONVERTIBLE', 'Convertible'),
+    ]
+    
+    car_type = models.CharField(max_length=15, choices=CAR_TYPES, default='SUV')
+    
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year}) - {self.car_type}"
+
+
+
+
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
 # - Name
